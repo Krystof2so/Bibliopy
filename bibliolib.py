8d1539ca62398003f679596fc62f70455c537179
Krystof <christofm@tutanota.com>
2021-06-13 14:50:51 +0200

    Changes in some classes and functions

--- bibliolib.py
+++ bibliolib.py
@@ -13,35 +13,35 @@ from const import DIR_DATAS
 class Bibliography:
     """Classe utilisée pour la création de la bibliographie et des répertoires idoines."""
     
-    def __init__(self, name_biblio):
+    def __init__(self, my_biblio):
         """Initialise.
 
         Args:
-            name_biblio (str): Nom de la bibliographie
+            my_biblio (str): Nom de la bibliographie
         """
-        self.name_biblio = name_biblio
-        self.dir_biblio = DIR_DATAS / str(name_biblio)
+        self.my_biblio = my_biblio
+        self.dir_biblio = DIR_DATAS / str(my_biblio)
     
-    def create_dir_biblio(self):
+    def create_dir_biblio(self) -> str:
         """Création des répertoires pour l'enregistrement des données.
 
         Returns:
             str: Nom de la bibliographie.
         """
-        if self.name_biblio in [b.name for b in DIR_DATAS.iterdir() if b.is_dir()]:
+        if self.my_biblio in [b.name for b in DIR_DATAS.iterdir() if b.is_dir()]: # in liste des répertoires dans .bibliopyDatas
             pass
         else: 
             self.dir_biblio.mkdir()
-        return self.name_biblio
+        return self.my_biblio
 
 
 class BookRef:
     """Classe pour définir la référence d'un livre selon les informations fournies."""
-    def __init__(self, book_datas):
+    def __init__(self, book_datas: dict):
         """Initialise.
 
         Args:
-            book_datas (dict): Informations sur un livre saisies par l'utilisateur.
+            book_datas (dict): Informations sur le livre saisi par l'utilisateur.
         """
         self.book_datas = book_datas
         self.type_doc = book_datas.get("document")
@@ -49,6 +49,12 @@ class BookRef:
         self.first_n_author = book_datas.get("prenom")
         self.book_title = book_datas.get("ouvrage")
         self.year = book_datas.get("an_parution")
+        self.mag = book_datas.get("revue")
+        self.start_page = book_datas.get("pp")
+        self.end_page = book_datas.get("pps")
+        self.editing = book_datas.get("edition")
+        self.nb_pages = book_datas.get("nb_pages")
+        self.isbn = book_datas.get("ISBN")
     
     def __str__(self):
         """Pour l'affichage.
@@ -70,22 +76,23 @@ class BookRef:
         t = self.book_title[:4]
         y = self.year
         ref = f"{td}{n}{p.lower()}{y}{t.capitalize()}"
-        return {ref: self.book_datas}
+        return ref
 
 
-class SaveBook:
+class SaveBook(BookRef):
     """Classe pour l'enregistrement des données dans les fichiers idoines."""
     
-    def __init__(self, book_data, my_biblio):
+    def __init__(self, ref_with_book, my_biblio, book_datas):
         """Initialise.
 
         Args:
-            book_data (dict): Informations sur un livre saisies par l'utilisateur.
-            my_biblio (str): Nom de la bibliogrpraphie.
+            ref_with_book (str): Référence du livre.
         """
-        self.dict_info = book_data
+        super().__init__(book_datas)
+        self.book_datas = book_datas
         self.my_biblio = my_biblio
-        self.file_json = DIR_DATAS / my_biblio / (self.my_biblio + ".json")
+        self.ref_with_book = ref_with_book
+        self.file_json = DIR_DATAS / self.my_biblio / (self.my_biblio + ".json")
     
     def check_json_file(self):
         """Vérification des références contenus dans le fichier .json idoine."""
@@ -104,25 +111,22 @@ class SaveBook:
         """
         with open(self.file_json, 'r') as file_b:
             checklist = json.loads(file_b.read())
-            for k in self.dict_info.keys():
-                if k not in checklist:
-                    checklist.append(k)
-                    with open(self.file_json, 'w') as file_b2:
-                        json.dump(checklist, file_b2, indent=4, ensure_ascii=False)
-                    return True
-                else:
-                    return False
+            if self.ref_with_book not in checklist:
+                checklist.append(self.ref_with_book)
+                with open(self.file_json, 'w') as file_b2:
+                    json.dump(checklist, file_b2, indent=4, ensure_ascii=False)
+                return True
+            else:
+                return False
     
     def save_livre_data(self):
         """Pour l'enregistrement dans la base de données (SQLite).
         
         Nécessité de retravailler cette fonction...
         """
-        dict_datadb = (list(self.dict_info.values())[0])
-        ref_of_book = (list(self.dict_info.keys())[0])
         biblio_database = sqlite3.connect(DIR_DATAS / "bibliography.db")
         cur = biblio_database.cursor()
-        if dict_datadb.get("document") == "A-":
+        if self.book_datas.get("document") == "A-":
             cur.execute("""
                         CREATE TABLE IF NOT EXISTS Articles(
                             Identifiant TEXT,
@@ -135,17 +139,17 @@ class SaveBook:
                             Theme TEXT
                             )
                         """)
-            dict_mag = {"Identifiant": ref_of_book, "Auteur": dict_datadb.get("nom"),
-                      "Prenom": dict_datadb.get("prenom"), "Titre": dict_datadb.get("ouvrage"),
-                      "Parution": dict_datadb.get("an_parution"), "Revue": dict_datadb.get("revue"),
-                      "Pages" : f"{dict_datadb.get('pp')} - {dict_datadb.get('pps')}",
+            dict_mag = {"Identifiant": self.ref_with_book, "Auteur": self.book_datas.get("nom"),
+                      "Prenom": self.book_datas.get("prenom"), "Titre": self.book_datas.get("ouvrage"),
+                      "Parution": self.book_datas.get("an_parution"), "Revue": self.book_datas.get("revue"),
+                      "Pages" : f"{self.book_datas.get('pp')} - {self.book_datas.get('pps')}",
                       "Theme": self.my_biblio}
             cur.execute("""
                         INSERT INTO Articles VALUES
                         (:Identifiant, :Auteur, :Prenom, :Titre, 
                         :Parution, :Revue, :Pages, :Theme)
                         """,dict_mag)
-        elif dict_datadb.get("document") == "L-":
+        elif self.book_datas.get("document") == "L-":
             cur.execute("""
                         CREATE TABLE IF NOT EXISTS Livres(
                             Identifiant TEXT,
@@ -159,16 +163,16 @@ class SaveBook:
                             Theme TEXT
                             )
                         """)
-            dict_book = {"Identifiant": ref_of_book, "Auteur": dict_datadb.get("nom"),
-                         "Prenom": dict_datadb.get("prenom"), "Titre": dict_datadb.get("ouvrage"),
-                         "Parution": dict_datadb.get("an_parution"), "Editeur": dict_datadb.get("edition"),
-                         "Pages" : dict_datadb.get("nb_pages"), "ISBN": dict_datadb.get("ISBN"),
+            dict_book = {"Identifiant": self.ref_with_book, "Auteur": self.book_datas.get("nom"),
+                         "Prenom": self.book_datas.get("prenom"), "Titre": self.book_datas.get("ouvrage"),
+                         "Parution": self.book_datas.get("an_parution"), "Editeur": self.book_datas.get("edition"),
+                         "Pages" : self.book_datas.get("nb_pages"), "ISBN": self.book_datas.get("ISBN"),
                          "Theme": self.my_biblio}
             cur.execute("""
                         INSERT INTO Livres VALUES
                         (:Identifiant, :Auteur, :Prenom, :Titre, 
                         :Parution, :Editeur, :Pages, :ISBN, :Theme)
-                        """,dict_book)
+                        """, dict_book)
         biblio_database.commit()
         cur.close()        
         biblio_database.close()
--- main.py
+++ main.py
@@ -16,4 +16,3 @@ if __name__ == "__main__":
             r = False
         else:
             r = True
-        
